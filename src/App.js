8241555f11b26894ce9cb33f0dce6c9a434413a4
jnsw <jns.w@icloud.com>
2020-11-20 18:10:53 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Axios from "axios"
 import Registration from "./components/auth/Registration"
