21aa66f23090c1b74a0123259be4ed1271968522
jnsw <jns.w@icloud.com>
2020-11-22 17:07:36 +0800

    pusher client

--- src/App.js
+++ src/App.js
@@ -6,71 +6,100 @@ import Axios from "axios"
 import Registration from "./components/auth/Registration"
 import Login from "./components/auth/Login"
 import {BrowserRouter as Router, Route, Switch, NavLink, useHistory, Redirect} from 'react-router-dom'
-import {Navbar,Nav,FaHome} from "react-bootstrap"
+import {Navbar, Nav, FaHome} from "react-bootstrap"
+import { tokenCheck } from "./functions/func";
+import Topbar from "./components/ui/topbar/Topbar";
 
 function App() {
 //states
-const [userInfo, setUserInfo] = useState({})
-const [isAuth, setIsAuth] = useState(false);
-let history = useHistory()
+  const [userInfo, setUserInfo] = useState({})
+  const [isAuth, setIsAuth] = useState(false);
+  let history = useHistory()
 
-function logout(){
-  localStorage.removeItem("token")
-  setUserInfo({})
-  setIsAuth(false)
-  history.push("/profile")
-}
+
+  useEffect(()=>{
+    tokenCheck(setIsAuth, setUserInfo)
+  },[])
+
+
+  function logout() {
+    localStorage.removeItem("token")
+    setUserInfo({})
+    setIsAuth(false)
+    history.push("/profile")
+  }
+
+
+
+
+  let welcomePage = ""
+  if (isAuth) {
+    welcomePage = [<div className="welcome-note-div">
+      <h2>Welcome back, {userInfo.username}!</h2>
+    </div>]
+  } else {
+    welcomePage = [
+      <div className="welcome-note-div">
+
+        <h2>Welcome to Apt Appt App, <br/> please login or register</h2>
+      </div>
+    ]
+  }
 
   return (
-  <Router>
+    <Router>
 
-<Navbar bg="light" expand="lg">
+      <Navbar bg="light" expand="lg">
 
-<Navbar.Brand  href="#home">AAA
-</Navbar.Brand>
+        <Navbar.Brand href="#home">AAA
+        </Navbar.Brand>
 
-<Navbar.Toggle aria-controls="basic-navbar-nav"/>
-<Navbar.Collapse id="basic-navbar-nav">
-  <Nav className="mr-auto">
-    
-
-  </Nav>
-  <Nav>{isAuth ?
-    <>
-    <Nav.Link href={`/profile/${userInfo.username}`}> <h2>{userInfo.username}</h2></Nav.Link> 
-    <Nav.Link className="mt-2 mr-auto" href="/">
-      something
-    </Nav.Link>
-
-    
-    <Nav.Link className="mt-2" color="black" href="/" onClick={logout}> something
-      </Nav.Link></> : <>
-    <Nav.Link href="/login"><h4>LOGIN</h4></Nav.Link>
-    <Nav.Link href="/register"><h4>REGISTER</h4></Nav.Link>
-    </>
-  }
-  </Nav>
-
-</Navbar.Collapse>
-</Navbar>
+        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
 
 
+          </Nav>
+          <Nav>{isAuth ?
+            <>
+              <Nav.Link href={`/profile/${userInfo.username}`}><h2>{userInfo.username}</h2></Nav.Link>
+              <Nav.Link className="mt-2 mr-auto" href="/">
+                something
+              </Nav.Link>
 
 
+              <Nav.Link className="mt-2" color="black" href="/" onClick={logout}> something
+              </Nav.Link></> : <>
+              <Nav.Link href="/login"><h4>LOGIN</h4></Nav.Link>
+              <Nav.Link href="/register"><h4>REGISTER</h4></Nav.Link>
+            </>
+          }
+          </Nav>
+
+        </Navbar.Collapse>
+      </Navbar>
 
 
-    {/* switch routes below */}
-    <Switch>
-    <Route path="/register" exact>
-      <Registration/>
-    </Route>
-    <Route path="/login">
-      <Login setUserInfo = {setUserInfo} setIsAuth ={setIsAuth}/>
-    </Route>
+      {/* switch routes below */}
+      <Switch>
+        <Route path="/" exact>
+          {welcomePage}
+        </Route>
+        <Route path="/topbar">
+          <Topbar/>
+        </Route>
 
 
-    </Switch>
-  </Router>
+        <Route path="/register" exact>
+          <Registration/>
+        </Route>
+        <Route path="/login">
+          <Login setUserInfo={setUserInfo} setIsAuth={setIsAuth}/>
+        </Route>
+
+
+      </Switch>
+    </Router>
 
   );
 }
@@ -80,7 +109,7 @@ export default App;
 
 // confirm compoenents needed
 
-//navbar
+//topbar
 
 //routes
 
---
+++ src/components/ui/searchbar/SearchSuggestion.jsx
@@ -0,0 +1,61 @@
+import React, {useEffect} from 'react';
+import {Link, NavLink, Route} from "react-router-dom";
+import Axios from 'axios'
+
+
+function SearchSuggestion({user, searchField, setShowSuggestions}) {
+
+
+  useEffect(() => {
+
+  })
+
+  async function addFriend(id){
+    try {
+      // **** hardcoded
+      await Axios.post('http://localhost:80/jonaswong/')
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+
+  const searchFieldArr = searchField.split(" ")
+  let id = user._id
+  let username = user.username
+  // let id = book['best_book'][0]['id'][0]['_']
+
+  // let author = book['best_book'][0]['author'][0]['name'][0]
+  // let imageUrl = book['best_book'][0]['image_url'][0]
+
+  for (const word of searchFieldArr) {
+    if (word !== "") {
+      let cleanWord = word.replace(/[^a-zA-Z0-9]/g, '') // clean up word of any illegal regex characters
+      let searchFieldRegex = new RegExp(cleanWord, 'gi')
+      username = username.replace(searchFieldRegex, "<strong><u>$&</u></strong>")
+      // author = author.replace(searchFieldRegex, "<strong><u>$&</u></strong>")
+    }
+  }
+
+  // author = "by " + author
+
+
+  return (
+
+    <Route>
+      <Link to={`/user/${id}`} onClick={()=>setShowSuggestions(false)}>
+        <div className="suggestion-card" id={id} key={id}>
+          {/*<img src={imageUrl} alt=""></img>*/}
+          <div className="suggestion-text">
+            <span dangerouslySetInnerHTML={{__html: username}}></span>
+          </div>
+          <button onClick={()=>addFriend(id)}>add</button>
+        </div>
+      </Link>
+    </Route>
+
+
+  );
+}
+
+export default SearchSuggestion;
